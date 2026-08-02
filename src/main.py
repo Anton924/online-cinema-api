e5396a1d9673b558d509fed86ba1251c67cd87f1
Anton <yakovenkoanton2007@gmail.com>
2026-08-02 14:44:16 +0300

    feat: add health check endpoint and fix CI python version

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI(
     title="Online Cinema API",
     description="FastAPI backend for browsing, purchasing and managing movies."
 )
 
+
+@app.get("/health/", status_code=status.HTTP_200_OK)
+def health(
+):
+    return {
+        "detail": "Connection configured!"
+    }
