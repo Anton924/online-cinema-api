d4c401b08d18be50f52749f875ba40910d55ada4
Anton <yakovenkoanton2007@gmail.com>
2026-08-04 01:05:17 +0300

    feat: Add JWT auth manager and password hashing

---
+++ src/security/__init__.py
@@ -0,0 +1,5 @@
+from security.passwords import (
+    hash_password,
+    verify_password
+)
+from security.utils import generate_secure_token
---
+++ src/security/token_manager.py
@@ -0,0 +1,67 @@
+from datetime import timedelta, datetime, timezone
+from typing import Optional
+
+import jwt
+from jwt import ExpiredSignatureError, PyJWTError
+from security.interfaces import JWTAuthManagerInterface
+
+from exceptions.security import TokenExpiredError, InvalidTokenError
+
+
+class JWTAuthManager(JWTAuthManagerInterface):
+
+    _ACCESS_KEY_TIMEDELTA_MINUTES = 60
+    _REFRESH_KEY_TIMEDELTA_MINUTES = 60 * 24 * 7
+
+    def __init__(self, secret_key_access: str, secret_key_refresh: str, algorithm: str):
+        self._secret_key_access = secret_key_access
+        self._secret_key_refresh = secret_key_refresh
+        self._algorithm = algorithm
+
+    def _create_token(self, data: dict, algorithm: str, secret_key: str, expires_delta: Optional[timedelta] = None):
+        to_encode = data.copy()
+        if expires_delta:
+            expire = datetime.now(timezone.utc) + expires_delta
+        else:
+            expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=algorithm)
+        return encoded_jwt
+
+    def create_access_token(self, data: dict, expires_delta: Optional[timedelta] = None) -> str:
+        return self._create_token(
+            data,
+            self._algorithm,
+            self._secret_key_access,
+            expires_delta or timedelta(minutes=self._ACCESS_KEY_TIMEDELTA_MINUTES)
+        )
+
+    def create_refresh_token(self, data: dict, expires_delta: Optional[timedelta] = None) -> str:
+        return self._create_token(
+            data,
+            self._algorithm,
+            self._secret_key_refresh,
+            expires_delta or timedelta(minutes=self._REFRESH_KEY_TIMEDELTA_MINUTES)
+        )
+
+    def decode_access_token(self, token: str) -> dict:
+        try:
+            return jwt.decode(token, self._secret_key_access, algorithms=[self._algorithm])
+        except ExpiredSignatureError:
+            raise TokenExpiredError
+        except PyJWTError:
+            raise InvalidTokenError
+
+    def decode_refresh_token(self, token: str) -> dict:
+        try:
+            return jwt.decode(token, self._secret_key_refresh, algorithms=[self._algorithm])
+        except ExpiredSignatureError:
+            raise TokenExpiredError
+        except PyJWTError:
+            raise InvalidTokenError
+
+    def verify_refresh_token_or_raise(self, token: str) -> None:
+        self.decode_refresh_token(token)
+
+    def verify_access_token_or_raise(self, token: str) -> None:
+        self.decode_access_token(token)
