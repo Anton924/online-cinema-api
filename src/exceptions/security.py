092b5f6ae3a92638e3e5fab8e5c9f7042c8f2fc5
Anton <yakovenkoanton2007@gmail.com>
2026-08-04 01:05:06 +0300

    feat: Add security exceptions for token handling

---
+++ src/exceptions/security.py
@@ -0,0 +1,21 @@
+class BaseSecurityError(Exception):
+    """Base class for all security-related errors."""
+
+    def __init__(self, message: str):
+        if message is None:
+            message = "A security error occurred."
+        super().__init__(message)
+
+
+class TokenExpiredError(BaseSecurityError):
+    """Raised when a token has expired."""
+
+    def __init__(self, message: str = "Token has expired."):
+        super().__init__(message)
+
+
+class InvalidTokenError(BaseSecurityError):
+    """Raised when a token is invalid."""
+
+    def __init__(self, message: str = "Invalid token."):
+        super().__init__(message)
