8e8f2bf5f46f2722826855ca7a6fa2bc75632f3c
Anton <yakovenkoanton2007@gmail.com>
2026-08-06 22:33:16 +0300

    feat: Add accounts request/response schemas

---
+++ src/schemas/accounts.py
@@ -0,0 +1,107 @@
+from datetime import datetime
+
+from pydantic import BaseModel, EmailStr, field_validator
+
+from database import accounts_validators
+from src.schemas.profiles import UserProfileResponseSchema
+
+
+class BaseEmailPasswordSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+    @field_validator("email")
+    @classmethod
+    def validate_email(cls, value):
+        return accounts_validators.validate_email(value)
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value):
+        return accounts_validators.validate_password(value)
+
+
+class UserRegistrationRequestSchema(BaseEmailPasswordSchema):
+    pass
+
+
+class UserRegistrationResponseSchema(BaseModel):
+    id: int
+    email: EmailStr
+    is_active: bool
+    created_at: datetime
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class UserActivationRequestSchema(BaseModel):
+    email: EmailStr
+    token: str
+
+
+class ResendActivationRequestSchema(BaseModel):
+    email: EmailStr
+
+
+class UserLoginRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+    @field_validator("email")
+    @classmethod
+    def validate_email(cls, value):
+        return accounts_validators.validate_email(value)
+
+
+class TokenPairResponseSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class LogoutRequestSchema(BaseModel):
+    refresh_token: str
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    refresh_token: str
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class PasswordChangeRequestSchema(BaseModel):
+    old_password: str
+    new_password: str
+
+
+class PasswordResetRequestSchema(BaseModel):
+    email: EmailStr
+
+
+class PasswordResetCompleteRequestSchema(BaseModel):
+    email: EmailStr
+    token: str
+    new_password: str
+
+
+class UserResponseSchema(BaseModel):
+    id: int
+    email: EmailStr
+    is_active: bool
+    group: str
+    created_at: datetime
+    updated_at: datetime
+    profile: UserProfileResponseSchema | None
