d42f298557744f10223944ba50861ed8c5464849
Anton <yakovenkoanton2007@gmail.com>
2026-08-18 15:41:23 +0300

    feat: Add email, JWT, and login configuration settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -11,6 +11,22 @@ class BaseAppSettings(BaseSettings):
     PATH_TO_DB: str = str(BASE_DIR / "database" / "source" / "movies.db")
     PATH_TO_MOVIES_CSV: str = str(BASE_DIR / "database" / "seed_db" / "imdb_movies.csv")
 
+    PATH_TO_EMAIL_TEMPLATES_DIR: str = str(BASE_DIR / "notifications" / "templates")
+    ACTIVATION_EMAIL_TEMPLATE_NAME: str = "activation_request.html"
+    ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME: str = "activation_complete.html"
+    PASSWORD_RESET_TEMPLATE_NAME: str = "password_reset_request.html"
+    PASSWORD_RESET_COMPLETE_TEMPLATE_NAME: str = "password_reset_complete.html"
+
+    LOGIN_TIME_DAYS: int = 7
+
+    EMAIL_HOST: str = "host"
+    EMAIL_PORT: int = 25
+    EMAIL_HOST_USER: str = "testuser"
+    EMAIL_HOST_PASSWORD: str = "test_password"
+    EMAIL_USE_TLS: bool = True
+    MAILHOG_API_PORT: int = 8025
+
+
 
 class Settings(BaseAppSettings):
     POSTGRES_USER: str = "test_user"
@@ -24,7 +40,10 @@ class Settings(BaseAppSettings):
     JWT_SIGNING_ALGORITHM: str = "HS256"
 
 
-class TestingSettings(BaseAppSettings):
+class TestingSettings(Settings):
+    SECRET_KEY_ACCESS: str = "SECRET_KEY_ACCESS"
+    SECRET_KEY_REFRESH: str = "SECRET_KEY_REFRESH"
+    JWT_SIGNING_ALGORITHM: str = "HS256"
 
     def model_post_init(self, __context: dict[str, Any] | None = None) -> None:
         object.__setattr__(self, "PATH_TO_DB", ":memory:")
