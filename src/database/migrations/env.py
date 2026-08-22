f8153a72d0ef887ad20080be0810f21695b90384
Anton <yakovenkoanton2007@gmail.com>
2026-08-22 17:11:14 +0200

    fix: Register movies models with Alembic autogeneration

--- src/database/migrations/env.py
+++ src/database/migrations/env.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 from alembic import context
 
 # from database.models import movies, accounts # noqa: F401
-from database.models import accounts
+from database.models import accounts, movies
 from database.models.base import Base
 from database.session_postgresql import sync_postgresql_engine
 
