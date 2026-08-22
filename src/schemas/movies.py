0c20abba93d4d3ea480d727200955af437ed53dc
Anton <yakovenkoanton2007@gmail.com>
2026-08-22 17:17:45 +0200

    feat: Add genre schemas

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,3 +1,6 @@
+from uuid import UUID
+from decimal import Decimal
+
 from pydantic import BaseModel, Field
 
 
@@ -8,6 +11,40 @@ class CertificationRequestSchema(BaseModel):
 class CertificationResponseSchema(BaseModel):
     id: int
     name: str
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class GenreRequestSchema(BaseModel):
+    name: str = Field(min_length=1, max_length=100)
+
+
+class GenreResponseSchema(BaseModel):
+    id: int
+    name: str
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class GenreWithMovieCountResponseSchema(BaseModel):
+    id: int
+    name: str
+    movie_count: int
+
+
+class MovieListItemResponseSchema(BaseModel):
+    id: int
+    uuid: UUID
+    name: str
+    year: int
+    time: int
+    imdb: float
+    price: Decimal
+    certification: CertificationResponseSchema
 
     model_config = {
         "from_attributes": True
