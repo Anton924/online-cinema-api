56098d1537e4d328b8f232ae51c16528707e213d
Anton <yakovenkoanton2007@gmail.com>
2026-08-22 17:13:47 +0200

    feat: Add certification schemas

---
+++ src/schemas/movies.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Field
+
+
+class CertificationRequestSchema(BaseModel):
+    name: str = Field(min_length=1, max_length=100)
+
+
+class CertificationResponseSchema(BaseModel):
+    id: int
+    name: str
+
+    model_config = {
+        "from_attributes": True
+    }
