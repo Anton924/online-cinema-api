bb77f87f1c3d853fa93225377f00121ea86ee3bf
Anton <yakovenkoanton2007@gmail.com>
2026-08-29 15:33:33 +0300

    feat: Add payment schemas

---
+++ src/schemas/payments.py
@@ -0,0 +1,49 @@
+from decimal import Decimal
+
+from pydantic import BaseModel
+
+from schemas.movies import MovieListItemResponseSchema
+
+from database.models.payments import PaymentStatus
+
+
+class PaymentItemResponseSchema(BaseModel):
+    id: int
+    movie: MovieListItemResponseSchema
+    price_at_payment: Decimal
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class PaymentResponseSchema(BaseModel):
+    id: int
+    status: PaymentStatus
+    external_payment_id: str
+    order_id: int
+    items: list[PaymentItemResponseSchema]
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class PaymentListItemResponseSchema(BaseModel):
+    id: int
+    status: PaymentStatus
+    order_id: int
+    items_count: int
+
+
+class PaymentAdminListItemResponseSchema(BaseModel):
+    id: int
+    status: PaymentStatus
+    order_id: int
+    user_email: str
+    items_count: int
+
+
+class PaymentSessionResponseSchema(BaseModel):
+    payment_id: int
+    checkout_url: str
